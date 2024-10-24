f90bdd1c1e22b59b9b2730eccbbed99bda410bcd
KiberYebok2009 <126006305+KiberYebok2009@users.noreply.github.com>
2024-10-24 13:49:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,109 +1,46 @@
-import pygame
 import random
 
-# Инициализация Pygame
-pygame.init()
+array = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
-# Константы
-WIDTH, HEIGHT = 800, 600
-BALL_RADIUS = 10
-PADDLE_WIDTH, PADDLE_HEIGHT = 100, 10
-BRICK_WIDTH, BRICK_HEIGHT = 75, 20
-FPS = 60
 
-# Цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+def game(arr):
+    c = 0
+    while c != 2:
+        print('Введите координаты (через пробел)')
+        str = input()
+        x = int(str[-1])
+        y = int(str[0])
+        if arr[x-1][y-1] == 1:
+            arr[x-1][y-1] = '+'
+            c+=1
+            print('Попадение!')
+        else:
+            arr[x - 1][y - 1] = '-'
+            print('Промах!')
+        for i in arr:
+            print(i)
+    print('ПОБЕДА!!!')
+    return 0
+def random_ships(arr):
+    while 1:
+        x1 = 0
+        y1 = 0
+        x2 = 0
+        y2 = 0
+        for i in range(2):
+            x1 = random.randint(0, 4)
+            y1 = random.randint(0, 4)
+            x2 = random.randint(0, 4)
+            y2 = random.randint(0, 4)
+            if [x1, y1] != [x2, y2]:
+                if [x2, y2] != [x1, y1 + 1] and [x2, y2] != [x1, y1 - 1] and [x2, y2] != [x1 + 1, y1] and [x2, y2] != [x1 - 1, y1]:
+                    arr[x1][y1] = 1
+                    arr[x2][y2] = 1
+                    return arr
 
-# Класс для мяча
-class Ball:
-    def __init__(self):
-        self.rect = pygame.Rect(WIDTH // 2 - BALL_RADIUS, HEIGHT // 2 - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
-        self.speed_x = random.choice([-4, 4])
-        self.speed_y = -4
 
-    def move(self):
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+new = random_ships(array)
+for i in new:
+    print(i)
 
-        # Отскок от стен
-        if self.rect.left <= 0 or self.rect.right >= WIDTH:
-            self.speed_x *= -1
-        if self.rect.top <= 0:
-            self.speed_y *= -1
-
-    def reset(self):
-        self.rect.center = (WIDTH // 2, HEIGHT // 2)
-        self.speed_x = random.choice([-4, 4])
-        self.speed_y = -4
-
-# Класс для ракетки
-class Paddle:
-    def __init__(self):
-        self.rect = pygame.Rect(WIDTH // 2 - PADDLE_WIDTH // 2, HEIGHT - PADDLE_HEIGHT - 10, PADDLE_WIDTH, PADDLE_HEIGHT)
-
-    def move(self, dx):
-        self.rect.x += dx
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-
-# Класс для кирпичей
-class Brick:
-    def __init__(self, x, y):
-        self.rect = pygame.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
-
-# Основная функция игры
-def main():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Arkanoid")
-    clock = pygame.time.Clock()
-
-    ball = Ball()
-    paddle = Paddle()
-    bricks = [Brick(x * (BRICK_WIDTH + 10) + 35, y * (BRICK_HEIGHT + 10) + 30) for x in range(10) for y in range(5)]
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            paddle.move(-10)
-        if keys[pygame.K_RIGHT]:
-            paddle.move(10)
-
-        ball.move()
-
-        # Проверка на столкновение с ракеткой
-        if ball.rect.colliderect(paddle.rect):
-            ball.speed_y *= -1
-
-        # Проверка на столкновение с кирпичами
-        for brick in bricks[:]:
-            if ball.rect.colliderect(brick.rect):
-                bricks.remove(brick)
-                ball.speed_y *= -1
-
-        # Проверка на падение мяча
-        if ball.rect.top >= HEIGHT:
-            ball.reset()
-
-        # Отрисовка объектов
-        screen.fill(BLACK)
-        pygame.draw.ellipse(screen, WHITE, ball.rect)
-        pygame.draw.rect(screen, WHITE, paddle.rect)
-        for brick in bricks:
-            pygame.draw.rect(screen, RED, brick.rect)
-
-        pygame.display.flip()
-        clock.tick(FPS)
-
-    pygame.quit()
-
-if __name__ == "__main__":
-    main()
+game(new)
